dfc0959b45eacb8426c708a581140491853c3192
Maasa <konomaasa@gmail.com>
2019-08-10 23:15:30 -0700

    Pet list with index

--- lib/spcala_pets/cli.rb
+++ lib/spcala_pets/cli.rb
@@ -1,7 +1,7 @@
 class CLI
 
     def call
-        adoptee_names = []
+        pet_names = []
         puts ""
         puts "Welcome, Animal Lover! The Society for the Prevention of Cruelty to Animals Los Angeles (spcaLA) has tons of furry friends who can't wait to be taken into a loving home.  Come check them out!"
 
@@ -9,11 +9,13 @@ class CLI
 
         doc = Nokogiri::HTML(html)
 
-        doc.css("#adoptList ul li h4")[2..184].each do |adoptee|        
-            name = adoptee.text
-            adoptee_names << name
+        doc.css("#adoptList ul li h4")[2..184].each do |pet|        
+            name = pet.text
+            pet_names << name
         end
 
-        binding.pry
+        pet_names.each.with_index(1) do |name, index|
+            puts "#{index}. #{name}"
+        end
     end
 end 
